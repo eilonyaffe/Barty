b3d72b3986ec5e049b3bf7afa613f9b81f3904d0
eilon <eilonyaffe3@gmail.com>
2025-06-24 20:53:32 +0300

    added truncation by tokens limit

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
 WAIT_SECS = 1
 RETRIES = 100
 TOKENS = 300
+TONE = 1  # 1: neutral as possible- saying the facts in support of the issue, 2: general opinion, 3: heated opinion, 4: humoristic opinion
--- src/sentence_transformer.py
+++ src/sentence_transformer.py
@@ -1,6 +1,7 @@
-# TODO functions to input the filtered articles' text + entities pairs, to output a formulated opinion using the LLM
 # TODO Omer recommended using dpsy tool, and gemini 2 flash model, maybe get a free key from google ai studio 
+
+from config import TONE
 
 from scraper import Article
 
-#TODO function which gets the Article class, and outputs
+#TODO function which gets the Article's instance, and outputs an opinion based on messages.json + TONE
